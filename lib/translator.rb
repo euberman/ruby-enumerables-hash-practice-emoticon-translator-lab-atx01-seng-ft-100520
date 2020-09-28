9c99004f0b2533094260775542643456c934b992
Eric Uberman <euberman@gmail.com>
2020-09-28 03:05:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,11 @@
 require "yaml"
 
 def load_library
-  YAML.load_file('emoticons.yml')
+  data = YAML.load_file('emoticons.yml')
+  emoticons = {}
+  data.each { |e_meaning, e_value|
+    emoticons[e_meaning]
+  
 end
 
 def get_japanese_emoticon
