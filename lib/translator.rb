30f1f090f1ef49e39110d9d0f327b45aa8b48ff5
Eric Uberman <euberman@gmail.com>
2020-09-28 03:43:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,18 +2,22 @@
 require "yaml"
 
 def load_library (file_location)
-  data = YAML.load_file(file_location)
   emoticons = {}
-  data.each { |e_key, e_value| emoticons[e_key] = {english: e_value[0], japanese: e_value[1]} }
+  YAML.load_file(file_location).each { |e_key, e_value| emoticons[e_key] = {english: e_value[0], japanese: e_value[1]} }
   puts emoticons
   emoticons
 end
 
 def get_japanese_emoticon (file_location, english_emoticon)
   data = load_library(file_location)
-  
+  data.each { |key,value| 
+    return value[:japanese] if value[:english] == english_emoticon
+  }
 end
 
 def get_english_meaning (file_location, japanese_emoticon)
-  
+  data = load_library(file_location)
+  data.each { |key,value| 
+    return key if value[:japanese] == japanese_emoticon
+  }
 end
