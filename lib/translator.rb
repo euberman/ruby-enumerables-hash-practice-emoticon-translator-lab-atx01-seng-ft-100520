dc56d5271b25675b18973f07db696a9b56ca383a
Eric Uberman <euberman@gmail.com>
2020-09-28 02:51:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,14 @@
 # require modules here
+require "yaml"
 
 def load_library
-  # code goes here
-  
-  lib/emoticons.yml
+  YAML.load_file('emoticons.yml')
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
-  # code goes here
+  
 end
